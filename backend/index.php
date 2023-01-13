47d8ebca07eb631aa45e686d8b494aa21f730a61
Piotr Witasik <piotrwitasik1988@gmail.com>
2023-01-13 18:14:56 +0100

    - rm proxy

--- backend/index.php
+++ backend/index.php
@@ -4,17 +4,14 @@
     header("Access-Control-Max-Age: 1000");
     header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
     header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
-    echo "TEST";
     $dbconn = pg_connect("host=localhost port=5432 dbname=car_rentals user=admin password=WsB12345!");
     if (!$dbconn) {
         die("ERRRORR: " . $dbconn);
     }
-    echo "CONNECTED";
 
     $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     $xml = file_get_contents('php://input');
     $requestMethod = $_SERVER["REQUEST_METHOD"];
     $e = explode( '/', $uri );
     echo $xml;
-
 ?>
