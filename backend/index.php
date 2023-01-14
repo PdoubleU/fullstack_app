f5be2dbc69a163c512a3d169c764ef67fafd2b90
Piotr Witasik <piotrwitasik1988@gmail.com>
2023-01-14 17:26:56 +0100

    - add missin php index

--- backend/index.php
+++ backend/index.php
@@ -4,14 +4,46 @@
     header("Access-Control-Max-Age: 1000");
     header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
     header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
-    $dbconn = pg_connect("host=localhost port=5432 dbname=car_rentals user=admin password=WsB12345!");
+
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $e = explode( '/', $uri);
+
+    $xml = file_get_contents('php://input');
+    $requestMethod = $_SERVER["REQUEST_METHOD"];
+    $obj;
+    $dbconn;
+
+    $obj = json_decode($xml);
+
+    if ($obj and $e) {    
+        // try to establish connection as administrator
+        $dbconn = pg_connect("host=localhost port=5432 dbname=car_rentals user=".$obj->credentials->login." password=".$obj->credentials->pwd."");
+        echo json_encode((object) [
+            'isAdmin' => true
+        ]);
+    } else {
+        // establish connection as standard user
+        $dbconn = pg_connect("host=localhost port=5432 dbname=car_rentals user=stduser password=user12345!");
+        echo json_encode((object) [
+            'isAdmin' => false
+        ]);
+    }
+    
     if (!$dbconn) {
         die("ERRRORR: " . $dbconn);
     }
 
-    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-    $xml = file_get_contents('php://input');
-    $requestMethod = $_SERVER["REQUEST_METHOD"];
-    $e = explode( '/', $uri );
-    echo $xml;
+    $qr = 'SELECT * FROM cars';
+
+    $result = pg_query($dbconn, $qr);
+
+
+    // $data = [];
+    // while ($row = pg_fetch_object($result)) {
+    //      $data[] = $row;
+    // }
+    // $response = [];
+    // $response['data'] = $data;
+
+    // echo json_encode($response, JSON_PRETTY_PRINT);
 ?>
